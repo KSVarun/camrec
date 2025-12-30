cdf0f2eccf91da361f597a7e1123e2f686ceb962
KSVarun <ksvarun001@gmail.com>
2025-12-30 13:47:08 +0530

    Update pi-camera-stream.sh to enhance audio streaming quality and change output format
    - Modified ffmpeg command to use libopus codec for audio, with a bitrate of 64k, single channel, and a sample rate of 48k.
    - Changed output format to mpegts and updated the streaming URL to use SRT protocol.

--- pi-camera-stream.sh
+++ pi-camera-stream.sh
@@ -8,7 +8,11 @@ CAMERA_RTSP="rtsp://192.168.1.2:554/ch0_0.h264"
 
 ffmpeg -rtsp_transport tcp \
 -i "$CAMERA_RTSP" \
--map 0:v:0 -c:v copy \
--map 0:a:0 -c:a copy \
--f rtsp \
-rtsp://127.0.0.1:8554/cam1
+-c:v copy \
+-c:a libopus \
+-ac 1 \
+-ar 48000 \
+-b:a 64k \
+-f mpegts \
+"srt://127.0.0.1:8890?mode=caller&latency=50&streamid=publish:cam1"
+
